9a0c8dc91fad8b716c9aad80baa8a7a77561ebe3
Lucas Meira <lima07.lucas@gmail.com>
2021-07-24 13:20:50 -0300

    declare getters and setters

--- entities.h
+++ entities.h
@@ -7,12 +7,36 @@ class Participant
 {
 private:
     Immatriculation _immatriculation;
-    Name _name;
+    Name _firstName;
     Name _lastName;
-    Email _emailAddress;
+    Email _email;
     PhoneNumber _phoneNumber;
     Password _password;
     Role _role;
+
+public:
+    Immatriculation setImmatriculation(Immatriculation);
+    Immatriculation getImmatriculation();
+
+    Name setFirstName(Name name);
+    Name getFirstName();
+
+    Name setLastName(Name name);
+    Name getLastName();
+
+    Name getFullName();
+
+    Email setEmail(Email email);
+    Email getEmail();
+
+    PhoneNumber setPhoneNumber(PhoneNumber phoneNumber);
+    PhoneNumber getPhoneNumber();
+
+    Password setPassword(Password password);
+    Password getPassword();
+
+    Role setRole(Role role);
+    Role getRole();
 };
 
 class Play
@@ -22,6 +46,19 @@ private:
     Name _name;
     PlayType _type;
     Rating _rating;
+
+public:
+    IdCode setId(IdCode id);
+    IdCode getId();
+
+    Name setName(Name name);
+    Name getName();
+
+    PlayType setType(PlayType playType);
+    PlayType getType();
+
+    Rating setRating(Rating rating);
+    Rating getRating();
 };
 
 class Session
@@ -30,6 +67,16 @@ private:
     IdCode _id;
     Date _date;
     Time _time;
+
+public:
+    IdCode setId(IdCode id);
+    IdCode getId();
+
+    Date setDate(Date date);
+    Date getDate();
+
+    Time setTime(Time time);
+    Time getTime();
 };
 
 class Room
@@ -38,6 +85,16 @@ private:
     IdCode _id;
     Name _name;
     Capacity _capacity;
+
+public:
+    IdCode setId(IdCode id);
+    IdCode getId();
+
+    Name setName(Name name);
+    Name getName();
+
+    Capacity setCapacity(Capacity capacity);
+    Capacity getCapacity();
 };
 
 #endif
