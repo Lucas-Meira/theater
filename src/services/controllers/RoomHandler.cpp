5def4c2ce9b9f50aa0705873b8cbd17ea96438e0
Pedro Rafael Faria Ferreira <58218137+pedr0o7@users.noreply.github.com>
2021-09-28 18:46:28 -0300

    Developing ROOM

---
+++ src/services/controllers/RoomHandler.cpp
@@ -0,0 +1,49 @@
+#include <sstream>
+
+#include "RoomHandler.h"
+#include "../db/DBHandler.h"
+
+SQLResult RoomHandler::create(const Room &room)
+{
+    std::stringstream query;
+
+    query << "INSERT INTO Rooms (id_rooms, name, capacities) VALUES ("
+          << '\'' << room.getId().get() << "\',"
+          << '\'' << room.getName().get() << "\',"
+          << '\'' << room.getCapacity().get() << "\');";
+
+    return DBHandler::getInstance()->execute(query);
+}
+
+SQLResult RoomHandler::remove(const Room &room)
+{
+    std::stringstream query;
+
+    query << "DELETE FROM Rooms WHERE id_rooms="
+          << '\'' << Room.getId().get() << "\';";
+
+    return DBHandler::getInstance()->execute(query);
+}
+
+SQLResult RoomHandler::search(const IdCode &id)
+{
+    std::stringstream query;
+
+    query << "SELECT id_rooms, name, rating, type  FROM Rooms WHERE id_rooms='" << id.get() << "';";
+
+    return DBHandler::getInstance()->execute(query);
+}
+
+SQLResult RoomHandler::update(const Room &room)
+{
+    std::stringstream query;
+
+    query << "UPDATE Rooms SET"
+          << "name = '"
+          << room.getName().get()
+          << "' capacities = '"
+          << room.getCapacity().get()
+          << "' WHERE id_rooms = '" << room.getId().get() << '\'';
+
+    return DBHandler::getInstance()->execute(query);
+}
